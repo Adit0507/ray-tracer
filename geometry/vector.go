a60a0472528522e57eabea77ec4037f112c16e17
Adit0507 <adi05072002@gmail.com>
2024-09-25 01:33:17 +0530

    methods on Vector

--- geometry/vector.go
+++ geometry/vector.go
@@ -20,3 +20,27 @@ func (v Vector) Normalize() Vector {
 	l := v.Length()
 	return Vector{v.X / l, v.Y / l, v.Z / l}
 }
+
+func (v Vector) Add(o Vector) Vector {
+	return Vector{v.X + o.X, v.Y + o.Y, v.Z + o.Z}
+}
+
+func (v Vector) Subtract(o Vector) Vector {
+	return Vector{v.X - o.X, v.Y - o.Y, v.Z - o.Z}
+}
+
+func (v Vector) AddScaler(t float64) Vector {
+	return Vector{v.X + t, v.Y + t, v.Z + t}
+}
+
+func (v Vector) SubtractScaler(t float64) Vector {
+	return Vector{v.X - t, v.Y - t, v.Z - t}
+}
+
+func (v Vector) MultiplyScaler(t float64) Vector {
+	return Vector{v.X *t, v.Y * t, v.Z * t}
+}
+
+func (v Vector) DivideScaler(t float64) Vector {
+	return Vector{v.X / t, v.Y / t, v.Z / t}
+}
