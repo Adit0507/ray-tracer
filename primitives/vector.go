09e2478a3e89ab1b4ec95f4ba54070c300d58d53
Adit0507 <adi05072002@gmail.com>
2024-09-25 22:28:26 +0530

    vector per unit in sphere for Lambertian reflectance

--- primitives/vector.go
+++ primitives/vector.go
@@ -2,10 +2,35 @@ package primitives
 
 import (
 	"math"
+	"math/rand"
 )
 
 type Vector struct {
 	X, Y, Z float64
+}
+
+var UnitVector = Vector{1, 1, 1}
+
+func VectorInUnitSphere() Vector {
+	for {
+		r := Vector{rand.Float64(), rand.Float64(), rand.Float64()}
+		p := r.MultiplyScaler(2.0).Subtract(UnitVector)
+		if p.SquaredLength() >= 1.0 {
+			return p
+		}
+	}
+}
+
+func (v Vector) Multiply(o Vector) Vector {
+	return Vector{v.X * o.X, v.Y * o.Y, v.Z * o.Z}
+}
+
+func (v Vector) Divide(o Vector) Vector {
+	return Vector{v.X / o.X, v.Y / o.Y, v.Z / o.Z}
+}
+
+func (v Vector) SquaredLength() float64 {
+	return v.X*v.X + v.Y*v.Y + v.Z*v.Z
 }
 
 func (v Vector) Length() float64 {
