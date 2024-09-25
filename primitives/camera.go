2e90570ed39105617dd0fd1cde3f160064bf0319
Adit0507 <adi05072002@gmail.com>
2024-09-25 13:11:50 +0530

    camera done

--- primitives/camera.go
+++ primitives/camera.go
@@ -1,17 +1,34 @@
 package primitives
 
 type Camera struct {
-	lowerLeft, horizontal, vertical, origin Vector	
+	lowerLeft, horizontal, vertical, origin Vector
 }
 
 func NewCamera() *Camera {
-	c :=new(Camera)
+	c := new(Camera)
 
-	c.lowerLeft  =Vector{-2, -1, -1}
-	c.horizontal = Vector{4,  0,0}
+	c.lowerLeft = Vector{-2, -1, -1}
+	c.horizontal = Vector{4, 0, 0}
 	c.vertical = Vector{0, 4, 0}
 	c.origin = Vector{0, 0, 0}
 
 	return c
 }
 
+func (c *Camera) position(u float64, v float64) Vector {
+	horizontal := c.horizontal.MultiplyScaler(u)
+	vertical := c.vertical.MultiplyScaler(v)
+
+	return horizontal.Add(vertical)
+}
+
+func (c *Camera) direction(position Vector) Vector {
+	return c.lowerLeft.Add(position)
+}
+
+func (c *Camera) RayAt(u float64, v float64) Ray {
+	position := c.position(u, v)
+	direction := c.direction(position)
+
+	return Ray{c.origin, direction}
+}
