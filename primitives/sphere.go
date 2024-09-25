c668f65592473b1015c787eb1561f5c79a22b5d9
Adit0507 <adi05072002@gmail.com>
2024-09-25 11:48:21 +0530

    info about hit

---
+++ primitives/sphere.go
@@ -0,0 +1,43 @@
+package primitives
+
+import "math"
+
+type Sphere struct {
+	Center Vector
+	Radius float64
+}
+
+type HitRecord struct {
+	T         float64
+	P, normal Vector
+}
+
+func (s *Sphere) Hit(r *Ray, tMin float64, tMax float64) (bool, HitRecord) {
+	oc := r.Origin.Subtract(s.Center)
+	a := r.Direction.Dot(r.Direction)
+	b := 2.0 * oc.Dot(r.Direction)
+	c := oc.Dot(oc) - s.Radius*s.Radius
+	discriminant := b*b - 4*a*c
+
+	rec := HitRecord{}
+
+	if discriminant > 0.0 {
+		t := (-b - math.Sqrt(b*b-a*c))/a
+		if t < tMax && t > tMin {
+			rec.T = t
+			rec.P = r.Point(t)
+            rec.normal = (rec.P.Subtract(s.Center)).DivideScaler(s.Radius)
+            return true, rec
+		}
+
+		t = (-b + math.Sqrt(b*b-a*c))/a
+		if t < tMax && t > tMin {
+			rec.T = t
+            rec.P = r.Point(t)
+            rec.normal = (rec.P.Subtract(s.Center)).DivideScaler(s.Radius)
+            return true, rec
+		}
+	}
+
+	return false, rec
+}
