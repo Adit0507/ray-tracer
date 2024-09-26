3fa6d2b06d90cc4106f841ac66000380a4bf47f9
Adit0507 <adi05072002@gmail.com>
2024-09-26 14:27:08 +0530

    refactor of addin all spheres at once

--- primitives/world.go
+++ primitives/world.go
@@ -8,6 +8,12 @@ func (w *World) Add(h Hitable) {
 	w.Elements = append(w.Elements, h)
 }
 
+func (w *World) AddAll(hitables ...Hitable) {
+	for _, h := range hitables {
+		w.Add(h)
+	}
+}
+
 func (w *World) Hit(r Ray, tMin float64, tMax float64) (bool, Hit) {
 	hitAnything := false
 	closest := tMax
